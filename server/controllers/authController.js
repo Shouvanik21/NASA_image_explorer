f218332d777a4a0c214ec77610168d5db23f09dd
Shouvanik21 <shouvanik210305@gmail.com>
2026-07-11 02:19:21 +0530

    created the basic server and connected it using mongoose and created a test route and controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,37 +1,47 @@
 const getCategories = async (req, res) => {
-  const categories = [
-    {
-      id: 1,
-      name: "Galaxies",
-      description: "Massive systems of stars, gas and dust.",
-    },
+  try {
+    const categories = [
+      {
+        id: 1,
+        name: "Galaxies",
+        description: "Massive systems of stars, gas and dust.",
+      },
 
-    {
-      id: 2,
-      name: "Nebulae",
-      description: "Clouds where new stars are born.",
-    },
+      {
+        id: 2,
+        name: "Nebulae",
+        description: "Clouds where new stars are born.",
+      },
 
-    {
-      id: 3,
-      name: "Planets",
-      description: "Worlds orbiting stars.",
-    },
+      {
+        id: 3,
+        name: "Planets",
+        description: "Worlds orbiting stars.",
+      },
 
-    {
-      id: 4,
-      name: "Star Clusters",
-      description: "Groups of stars held together by gravity.",
-    },
+      {
+        id: 4,
+        name: "Star Clusters",
+        description: "Groups of stars held together by gravity.",
+      },
 
-    {
-      id: 5,
-      name: "Supernovae",
-      description: "Exploding stars.",
-    },
-  ];
+      {
+        id: 5,
+        name: "Supernovae",
+        description: "Exploding stars.",
+      },
+    ];
 
-  res.json(categories);
+    res.status(200).json({
+      message: "categories found",
+      category: categories,
+    });
+  } catch {
+    (err) =>
+      res.status(500).json({
+        message: err.message,
+      });
+  }
 };
 
 module.exports = {
