f135ac94f0842e673d1957bd84a6a7b00f89fc06
Shouvanik21 <shouvanik210305@gmail.com>
2026-07-11 18:09:43 +0530

    backend created and tested using postman

---
+++ server/controllers/getImages.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+
+const getImages = async (req, res) => {
+  try {
+    const search = req.params.search;
+
+    const response = await axios.get("https://images-api.nasa.gov/search", {
+      params: {
+        q: search,
+        media_type: "image",
+      },
+    });
+
+    //GET ALL IMAGES
+    const images = response.data.collection.items;
+    // console.log(images.length)
+
+    // //CONVERT NASA'S RESPONSE TO AN ARRAY
+    const formattedImages = images.slice(0, 5).map((image) => {
+      return {
+        title: image.data?.[0]?.title,
+        description: image.data?.[0]?.description,
+        imageUrl: image.links?.[0]?.href,
+        date: image.data?.[0]?.date_created,
+      };
+    });
+
+    res.json(formattedImages);
+  } catch {
+    (err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "failed to fetch images",
+      });
+    };
+  }
+};
+
+module.exports = { getImages };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,8 +2,10 @@ const express=require("express")
 
 const router=express.Router()
 
-const {getCategories}=require("../controllers/authController")
+const {getCategories}=require("../controllers/getCategory")
+const {getImages}=require("../controllers/getImages")
 
 router.get("/category",getCategories)
+router.get("/getimages/:search",getImages)
 
 module.exports=router
